555c0ecfc791a2f585cb64cddb8a3740831decbe
jacques-onek <onekongajaque@gmail.com>
2025-09-01 05:02:32 +0100

    je travaile sur le header du home

--- components/home/Header.tsx
+++ components/home/Header.tsx
@@ -9,14 +9,14 @@ const Header = () => {
     const date = (new Date().toDateString())
 
   return (
-    <Card  className='flex justify-between px-4 flex-row  border border-slate-200 rounded-md bg-[#edfaf9ee] hover:shadow transition duration-600 w-full   text-black py-6 '>
+    <Card  className='flex justify-between px-4 flex-row  border border-slate-200 rounded-md bg-[#edfaf9ee] hover:shadow transition duration-600     text-black py-6 '>
        <div className='flex flex-col gap-2'>
-          <h1 className='font-extrabold text-3xl'>Bienvenue  <span className='text-blue-600'>{patients.prenom}</span>  </h1>
-          <p>Voici un aperçu de votre suivi médical</p>
+          <h1 className='font-bold text-3xl'>Bienvenue  <span className='text-blue-600'>{patients.prenom}</span>  </h1>
+          <p className='font-sans'>Voici un aperçu de votre suivi médical</p>
        </div>
        <div>
          <p>aujourdhui</p>
-          <h2 className='font-extrabold text-lg'>{date} </h2>
+          <h2 className='font-extrabold text-lg max-md:text-[1.8rem]'>{date} </h2>
        </div>
     </Card>
   )
