89c62cec8426182f2c1b15e8278f7784f62e2a9f
jacques-onek <onekongajaque@gmail.com>
2025-09-02 12:18:04 +0100

    developpent de lq page dossier

--- app/dossier/page.tsx
+++ app/dossier/page.tsx
@@ -1,10 +1,42 @@
+import { dossiers } from '@/constant/AppData'
+import StatsContainer from '@/components/StatsContainer';
+import {  Unlink, FileText, TriangleAlert } from 'lucide-react'
 import React from 'react'
+import AntecedantContainer from '@/components/dossiers/AntecedantContainer';
 
 const page = () => {
+
+
+
+  const statsEntecedant = dossiers[0].antecedents.length
+  const statsAllergies = dossiers[0].allergies.length
+  const statsMedications = dossiers[0].traitements.length
+
+  const allDossierStats = [
+    {
+      icons:FileText,
+      score:statsEntecedant,
+      description:"Antécédents medicaux",
+      circleBg:"bg-purple-100 text-purple-700"
+    },
+    {
+      icons:TriangleAlert,
+      score:statsAllergies,
+      description:"Allergies",
+      circleBg:"bg-red-100 text-red-700"
+    },
+    {
+      icons:Unlink,
+      score:statsMedications,
+      description:"Traitements en cours",
+      circleBg:"bg-indigo-100 text-indigo-700"
+    }
+  ]
   return (
-    <div>
-      
-    </div>
+    <main className='container lg:w-[77vw]'>
+       <StatsContainer data={allDossierStats}/>
+       <AntecedantContainer/>
+    </main>
   )
 }
 
